031eb62d5b9b4f0e1f84ff5208e4494654e9e5cc
Alejandro González Gutiérrez <aggutierrez@plainconcepts.com>
2023-02-01 09:57:43 +0100

    Add Serilog

--- AzCogniServ.Api/Program.cs
+++ AzCogniServ.Api/Program.cs
@@ -1,34 +1,78 @@
 using Hangfire;
 using Hangfire.SqlServer;
+using Serilog;
+using Serilog.Events;
 
-var builder = WebApplication.CreateBuilder(args);
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
 
-builder.Services.AddHangfire(configuration => configuration
-    .SetDataCompatibilityLevel(CompatibilityLevel.Version_170)
-    .UseSimpleAssemblyNameTypeSerializer()
-    .UseRecommendedSerializerSettings()
-    .UseSqlServerStorage(builder.Configuration.GetConnectionString("Default"), new SqlServerStorageOptions
+try
+{
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Host.UseSerilog((context, services, configuration) =>
     {
-        CommandBatchMaxTimeout = TimeSpan.FromMinutes(5),
-        SlidingInvisibilityTimeout = TimeSpan.FromMinutes(5),
-        QueuePollInterval = TimeSpan.Zero,
-        UseRecommendedIsolationLevel = true,
-        DisableGlobalLocks = true
-    })
-);
+        configuration
+            .ReadFrom.Configuration(context.Configuration)
+            .ReadFrom.Services(services)
+            .Enrich.FromLogContext()
+            .WriteTo.Console();
 
-builder.Services.AddHangfireServer();
+        if (builder.Environment.IsProduction())
+        {
+            // Add Azure Diagnostics Log Stream
+            configuration.WriteTo.File(
+                Path.Combine(Environment.GetEnvironmentVariable("HOME") ?? "/", "LogFiles", "Application", "diagnostics.txt"),
+                rollingInterval: RollingInterval.Day,
+                fileSizeLimitBytes: 10 * 1024 * 1024,
+                retainedFileCountLimit: 2,
+                rollOnFileSizeLimit: true,
+                shared: true,
+                flushToDiskInterval: TimeSpan.FromSeconds(1));
+        }
+    });
 
-var app = builder.Build();
+    builder.Services.AddHangfire(configuration => configuration
+        .SetDataCompatibilityLevel(CompatibilityLevel.Version_170)
+        .UseSimpleAssemblyNameTypeSerializer()
+        .UseRecommendedSerializerSettings()
+        .UseSerilogLogProvider()
+        .UseSqlServerStorage(builder.Configuration.GetConnectionString("Default"), new SqlServerStorageOptions
+        {
+            CommandBatchMaxTimeout = TimeSpan.FromMinutes(5),
+            SlidingInvisibilityTimeout = TimeSpan.FromMinutes(5),
+            QueuePollInterval = TimeSpan.Zero,
+            UseRecommendedIsolationLevel = true,
+            DisableGlobalLocks = true
+        })
+    );
 
-app.UseHttpsRedirection()
-    .UseRouting()
-    .UseHangfireDashboard()
-    .UseEndpoints(endpoints => endpoints.MapHangfireDashboard());
+    builder.Services.AddHangfireServer();
 
-using var scope = app.Services.CreateScope();
-var backgroundJobsClient = scope.ServiceProvider.GetRequiredService<IBackgroundJobClient>();
+    var app = builder.Build();
 
-backgroundJobsClient.Enqueue(() => Console.WriteLine("Hello world from Hangfire!"));
+    app.UseHttpsRedirection()
+        .UseRouting()
+        .UseHangfireDashboard()
+        .UseEndpoints(endpoints => endpoints.MapHangfireDashboard());
 
-app.Run();
+    using var scope = app.Services.CreateScope();
+    var backgroundJobsClient = scope.ServiceProvider.GetRequiredService<IBackgroundJobClient>();
+
+    backgroundJobsClient.Enqueue(() => Console.WriteLine("Hello world from Hangfire!"));
+
+    app.Run();
+    return 0;
+}
+catch (Exception e)
+{
+    Log.Fatal(e, "Host failed to start");
+    return 1;
+}
+finally
+{
+    Log.CloseAndFlush();
+}
