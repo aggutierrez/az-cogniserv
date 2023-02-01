55b7265b0531a461bc5b1fb3cad2163a69e29575
Alejandro González Gutiérrez <aggutierrez@plainconcepts.com>
2023-02-01 09:47:40 +0100

    Add Hangfire

--- AzCogniServ.Api/Program.cs
+++ AzCogniServ.Api/Program.cs
@@ -1,25 +1,34 @@
+using Hangfire;
+using Hangfire.SqlServer;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddHangfire(configuration => configuration
+    .SetDataCompatibilityLevel(CompatibilityLevel.Version_170)
+    .UseSimpleAssemblyNameTypeSerializer()
+    .UseRecommendedSerializerSettings()
+    .UseSqlServerStorage(builder.Configuration.GetConnectionString("Default"), new SqlServerStorageOptions
+    {
+        CommandBatchMaxTimeout = TimeSpan.FromMinutes(5),
+        SlidingInvisibilityTimeout = TimeSpan.FromMinutes(5),
+        QueuePollInterval = TimeSpan.Zero,
+        UseRecommendedIsolationLevel = true,
+        DisableGlobalLocks = true
+    })
+);
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddHangfireServer();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseHttpsRedirection()
+    .UseRouting()
+    .UseHangfireDashboard()
+    .UseEndpoints(endpoints => endpoints.MapHangfireDashboard());
 
-app.UseHttpsRedirection();
+using var scope = app.Services.CreateScope();
+var backgroundJobsClient = scope.ServiceProvider.GetRequiredService<IBackgroundJobClient>();
 
-app.UseAuthorization();
-
-app.MapControllers();
+backgroundJobsClient.Enqueue(() => Console.WriteLine("Hello world from Hangfire!"));
 
 app.Run();
