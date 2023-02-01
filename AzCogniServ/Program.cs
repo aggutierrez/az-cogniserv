9f00c04d0d5c23a5ae2fe84994d5fc69c2ea6327
Alejandro González Gutiérrez <aggutierrez@plainconcepts.com>
2023-02-01 09:07:47 +0100

    Implement sample image analysis

--- AzCogniServ/Program.cs
+++ AzCogniServ/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
+
+const string compvKey = "8c82a1d3adf84c5a984b854075f2a1d9";
+const string compvEndpoint = "https://kandu-comv.cognitiveservices.azure.com/";
+
+using var client = new ComputerVisionClient(new ApiKeyServiceClientCredentials(compvKey)) { Endpoint = compvEndpoint };
+var imageUrl = "https://moderatorsampleimages.blob.core.windows.net/samples/sample16.png";
+
+Console.WriteLine($"Analyzing image [{imageUrl}]...");
+
+var results = await client.AnalyzeImageAsync(imageUrl,
+    visualFeatures: new List<VisualFeatureTypes?>
+    {
+        VisualFeatureTypes.Tags,
+        VisualFeatureTypes.Categories,
+        VisualFeatureTypes.Color,
+        VisualFeatureTypes.Adult,
+        VisualFeatureTypes.Brands,
+        VisualFeatureTypes.Description,
+        VisualFeatureTypes.Faces,
+        VisualFeatureTypes.ImageType
+    });
+
+if (results.Tags.Count == 0)
+{
+    Console.WriteLine("No tags found");
+    return;
+}
+
+Console.WriteLine($"Found [{results.Tags.Count}] tags:");
+Console.WriteLine();
+Console.WriteLine("| Tag name -> Tag confidence |");
+
+foreach (var tag in results.Tags)
+    Console.WriteLine($"| {tag.Name}:   {tag.Confidence} |");
+
+Console.WriteLine();
+Console.WriteLine($"Found [{results.Categories.Count}] categories:");
+Console.WriteLine();
+Console.WriteLine("| Category name -> Category confidence |");
+
+foreach (var category in results.Categories)
+    Console.WriteLine($"| {category.Name}:   {category.Score} |");
